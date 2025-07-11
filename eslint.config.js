a11f021e670a6919df89d5003db4d7d97d88195d
Stan Trotsko <stan@Stans-MacBook-Pro.local>
2025-07-11 14:35:02 +0600

    First commit

--- eslint.config.js
+++ eslint.config.js
@@ -22,32 +22,3 @@ module.exports = [
     },
   },
 ];
-
-
-
-// const tsPlugin = require("@typescript-eslint/eslint-plugin");
-// const tsParser = require("@typescript-eslint/parser");
-
-// /** @type {import("eslint").Linter.Config[]} */
-// module.exports = [
-//   {
-//     files: ["**/*.ts", "**/*.js"],
-//     languageOptions: {
-//       parser: tsParser,
-//       parserOptions: {
-//         ecmaVersion: 2020,
-//         sourceType: "module",
-//       },
-//     },
-//     plugins: {
-//       "@typescript-eslint": tsPlugin,
-//     },
-//     rules: {
-//       'indent': ['error', 2],
-//       'semi': ['error', 'always'],
-//       'no-multiple-empty-lines': ['error', { max: 1 }],
-//       "prefer-const": "warn",
-//       "@typescript-eslint/no-unused-vars": "warn",
-//     },
-//   },
-// ];
