e2e00dbb79bd9ce460143954a25c46d6e2178f3c
Stan Trotsko <stan@Stans-MacBook-Pro.local>
2025-07-11 22:25:33 +0600

    Add README and Log file

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import { CsvLogger } from './logger/CsvLogger.js';
 import { CATEGORY_LIST } from './types/common.js';
 import { formatDuration } from './utils/DateTime.js';
 
-const TOTAL_LEADS = 100;
-const ASYNC_LIMIT = 20;
+const TOTAL_LEADS = 10000;
+const ASYNC_LIMIT = 34;
 
 const generator = new LeadGenerator(CATEGORY_LIST);
 const logger = new CsvLogger();
